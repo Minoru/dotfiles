108a535ddba544604c5f80695c3dfbb6e7093d13
Alexander Batischev <eual.jp@gmail.com>
2018-01-03 20:05:55 +0300

    Get rid of local newsbeuter-dev package

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,7 +1,6 @@
 { pkgs }: {
 
   packageOverrides = super: let self = super.pkgs; in with self; rec {
-    newsbeuter-dev = callPackage ./pkgs/newsbeuter { };
     newsboat-dev = callPackage ./pkgs/newsboat { };
   };
 }
