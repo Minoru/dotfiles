f8e21d32a4f91c2ffbda0e7be3e5af9befaea604
Alexander Batischev <eual.jp@gmail.com>
2024-10-04 18:55:13 +0300

    Remove unused scripts
    And ignore the scripts directory because now it only contains
    machine-specific scripts that I don't want to replicate across devices.

--- .scripts/crontab/update_trackball_baterry_usage_log.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-(
-    echo -n "`date +\%s` "
-    sudo $HOME/.nix-profile/bin/solaar-cli show --verbose 1 | grep 'Battery' | sed -r 's#^[^[:digit:]]*([[:digit:]]+\%).*$#\1#'
-) >> /home/minoru/misc/trackball_battery_usage.log
